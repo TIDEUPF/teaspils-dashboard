d057afe140539b303812b1773925e1f6c6a7bdfe
rferrers <rferrers@gmail.com>
2021-06-02 11:30:49 +0200

    Initial Commit: Basic Desgin of the Dashboard. Back-end still to be developed.
    Second Commit:
    Initialization of Docker.
    Improvement of the Temfun javascript file.
    Adding the conditionals for the colors.

--- js/Term_fun.js
+++ js/Term_fun.js
@@ -3,98 +3,111 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+window.addEventListener("load", () =>
+{
 
-/*
-* PREGUNTES:
-* Perk la pagina te el slider d'abaix?
-* Fer-lo responsive?
-*
-* */
-/*We initialize the variables as global*/
-var Real_date = "hello" ; 
-var Real_Temperature = 15; 
-var Real_CO2 = 15;  
-var Real_Light = 15;  
-var Real_Humidity = 15; 
-var Real_Noise = 15; 
+ /*We generate a request to get the .json*/
+ const section = document.querySelector('#temp');
+ /*Almacenamos la dirección URL del json*/
+ const requestURL = 'https://raw.githubusercontent.com/rferrers/Teaspils/main/DataAll.json';
+ /*const requestURL = 'https://raw.githubusercontent.com/rferrers/Teaspils/main/DataAll2.json';*/
 
-/*We generate a request to get the .json*/
-const section = document.querySelector('#temp');
-/*Almacenamos la dirección URL del json*/
-const requestURL = 'https://raw.githubusercontent.com/rferrers/Teaspils/main/DataAll.json';
-/*const requestURL = 'https://raw.githubusercontent.com/rferrers/Teaspils/main/DataAll2.json';*/
+ /*Creamos una nueva instancia de la clase XMLHttpRequest()*/
+ const request = new XMLHttpRequest();
+ /*Con el open hacemos una solicitud del Request*/
+ request.open('GET', requestURL);
+ /*Definimos que el tipo de archivo que usaremos sera un json*/
+ request.responseType = 'json';
+ request.send();
+ /*Cómo manejar la respuesta del servidor*/
+ request.onload = function() {
 
-/*Creamos una nueva instancia de la clase XMLHttpRequest()*/
-const request = new XMLHttpRequest();
-/*Con el open hacemos una solicitud del Request*/
-request.open('GET', requestURL);
-/*Definimos que el tipo de archivo que usaremos sera un json*/
-request.responseType = 'json';
-request.send();
-/*Cómo manejar la respuesta del servidor*/
-request.onload = function() {
-const Data = request.response;
-/*showTemp(Temperatures);
-}   */
+  var Data = request.response;
+  loadDashboard(Data)
+  /*showTemp(Temperatures);
+  }   */
 
-const Date = Data['Date'];
-window.Real_date = Date[0].Value;
+ };
 
-const Temperature = Data['Temperature'];
-window.Real_Temperature = Temperature[0].Value;
-
-const Noise = Data['Noise'];
-window.Real_Noise = Noise[0].Value;
-
-const CO2 = Data['CO2'];
-window.Real_CO2 = CO2[0].Value;
-
-const Light = Data['Light'];
-window.Real_Light = Light[0].Value;
-
-const Humidity = Data['Humidity'];
-window.Real_Humidity = Humidity[0].Value;
-
-};
+ document.body.classList.add("loaded"); 
+});
 
 const red = '#FF3104';
 const green = '#4AD347';
 const orange = '#E8B147';
 
-window.addEventListener("load", () => 
-{
- 
+function loadDashboard(Data){
+
  var Date_place = document.getElementById("date");
- 
- const myH2 = document.createElement('h3');
- myH2.textContent = Real_date;
+
+ var myH2 = document.createElement('h3');
+ myH2.textContent = Data['Date'][0].Value;
  Date_place.appendChild(myH2);
- 
- 
- var Temperature = document.getElementById("T-load");
- const valueT = Math.round((Real_Temperature/40)*100);
+
+ let Temperature = document.getElementById("T-load");
+ let valueT = Math.round((Data['Temperature'][0].Value/40)*100);
  Temperature.style.height = valueT.toString()+'%' ;
- Temperature.style.backgroundColor = green;
- 
+ let color_T
+ if (valueT < 60) {
+  color_T = green;
+ } else if (valueT < 80){
+  color_T  = orange;
+ } else {
+  color_T = red;
+ }
+ Temperature.style.backgroundColor = color_T;
+
+
  var Noise = document.getElementById("N-load");
- const valueN = Math.round((Real_Noise/100)*100);
+ var valueN = Math.round((Data['Noise'][0].Value/100)*100);
  Noise.style.height = valueN.toString()+'%' ;
- Noise.style.backgroundColor = green;
- 
+
+ let color_N
+ if (valueN < 60) {
+  color_N = green;
+ } else if (valueN < 80){
+  color_N = orange;
+ } else {
+  color_N = red;
+ }
+ Noise.style.backgroundColor = color_N;
+
  var CO2 = document.getElementById("CO2-load");
- const valueC = Math.round((Real_CO2/2000)*100);
+ var valueC = Math.round((Data['CO2'][0].Value/2000)*100);
  CO2.style.height = valueC.toString()+'%';
- CO2.style.backgroundColor = red;
- 
+ let color_C
+ if (valueC < 60) {
+  color_C = green;
+ } else if (valueC < 80){
+  color_C = orange;
+ } else {
+  color_C = red;
+ }
+ CO2.style.backgroundColor = color_C;
+
  var Hum = document.getElementById("H-load");
- const valueH = Math.round(Real_Humidity);
+ var valueH = Math.round(Data['Humidity'][0].Value);
  Hum.style.height = valueH.toString()+'%';
- Hum.style.backgroundColor = orange;
- 
+ let color_H
+ if (valueH < 60) {
+  color_H = green;
+ } else if (valueH < 80){
+  color_H = orange;
+ } else {
+  color_H = red;
+ }
+ Hum.style.backgroundColor = color_H;
+
  var Light = document.getElementById("L-load");
- const valueL = Math.round((Real_Light/2000)*100);
+ var valueL = Math.round((Data['Light'][0].Value/2000)*100);
  Light.style.height = valueL.toString()+'%';
- Light.style.backgroundColor = green;
- 
- document.body.classList.add("loaded"); 
-});
+ let color_L
+ if (valueL < 60) {
+  color_L = green;
+ } else if (valueL < 80){
+  color_L = orange;
+ } else {
+  color_L = red;
+ }
+ Light.style.backgroundColor = color_L;
+}

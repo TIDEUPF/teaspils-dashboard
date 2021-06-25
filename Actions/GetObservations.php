7e7865f7a7397c7dd913428daad30f0c201abc13
rferrers <rferrers@gmail.com>
2021-06-25 14:42:05 +0200

    Initial Commit: Basic Desgin of the Dashboard. Back-end still to be developed.
    Second Commit:
    Initialization of Docker.
    Improvement of the Temfun javascript file.
    Adding the conditionals for the colors.
    
    Third Commit:
    
    Added the export file functionality and the update limits.
    Changed the footer.
    
    Fourth Commit:
    
    Added working comments.
    
    Fifth Commit:
    
    The setting button.
    Added all possible variables and change the settings place.
    
    Sixth Commit:
    Setting button updated.
    First steps with the db service.
    
    Seventh Commit:
    
    Working DataBase

---
+++ Actions/GetObservations.php
@@ -0,0 +1,25 @@
+<?php
+include ('../DB_Connection.php');
+
+
+
+/*or isset*/
+if (empty($_POST['IdPlant'])){
+    header("HTTP/1.0 404 IdPlant not found");
+    exit; /*Ya no se ejecuta más*/
+}
+$Id = $_POST['IdPlant'];
+global $conn;
+$Result = mysqli_query($conn, "SELECT * FROM PLANTS_INFO_DB WHERE IdPlant = '$Id'");
+if(mysqli_num_rows($Result)>0){
+    $DataJson = array();//Creamos una array vacía
+    while($row = mysqli_fetch_assoc($Result)) {
+        $DataJson[] = $row; //Le añadimos elementos a la array, donde cada posición sera un elemento.
+    }
+    echo json_encode($DataJson);
+}
+else{
+    header("HTTP/1.0 404 No observations found");/*Ya no se ejecuta más*/
+}
+exit;
+
--- js/Observation_fun.js
+++ js/Observation_fun.js
@@ -2,12 +2,16 @@
 
 var Current_day = "Hello";
 window.addEventListener("load", () => {
-    let comment = 'Please right some of your thoughts in here...';
-    console.log(comment)
+
+    let IdPlant = 1234;
+    let comment = 'Please write some of your thoughts in here...';
     var Date_place = document.getElementById("date");
     var f = new Date();
     window.Current_day = f.getDate() + "/" + (f.getMonth() + 1) + "/" + f.getFullYear();
     let Today = window.Current_day + ' ' + f.getHours() + ':' + f.getMinutes() + ':' + f.getSeconds();
+    /*****CONECTAMOS CON LA DB*******/
+    checkDBcomments(IdPlant)
+
 
     const myH2 = document.createElement('h3');
     myH2.textContent = Current_day;
@@ -20,19 +24,62 @@ window.addEventListener("load", () => {
     input_button.addEventListener('click', function () {
         let written_value = written_input.value
         console.log(written_value)
-        loadComments(written_value)
+        saveComment(written_value)
+
     })
 
-    function loadComments( written_value){
+  function loadComments(written_value) {
 
-        var f = new Date();
+        let f = new Date();
         let Today = f.getDate() + "/" + (f.getMonth() + 1) + "/" + f.getFullYear() + ' ' + f.getHours() + ':' + f.getMinutes() + ':' + f.getSeconds();
-        var Date_place = document.getElementById("comment_container");
+        let Date_place = document.getElementById("comment_container");
         let myDate = document.createElement('h2');
         let myH2 = document.createElement('h3');
         myDate.textContent = Today;
         myH2.textContent = written_value;
         Date_place.appendChild(myDate);
         Date_place.appendChild(myH2);
-};
+    };
+
+
+    function saveComment(written_value) {
+        let Info_Json = {IdPlant: IdPlant, Name: 'Student' , Text: written_value, Image: ' '  }
+        $.ajax({
+            url: 'Actions/AddObservation.php',
+            method: 'post',
+            data: {Info_Json: Info_Json},
+            success: function (DataJson) {
+                loadComments(written_value)
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        });
+    };
+
+    function display(Json_string) {
+        let Json_data = JSON.parse(Json_string)
+        Object.values(Json_data).forEach(obs=>{
+            console.log(obs)
+        })
+        //Json_data.forEach(obs => {
+        //    console.log(Json_data)
+        //});
+    }
+
+    function checkDBcomments(IdPlant) {
+        $.ajax({
+            url: 'Actions/GetObservations.php',
+            method: 'post',
+            data: {IdPlant: IdPlant},
+            success: function (DataJson) {
+                display(DataJson)
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+
+        });
+
+    }
 });

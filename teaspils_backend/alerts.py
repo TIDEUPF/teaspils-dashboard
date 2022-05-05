0926595eb83593077d90d1d078b22a9a5d08e424
Paula Balcells <paula.balcells01@estudiant.upf.edu>
2022-05-05 14:00:29 +0200

    update advising condition alerts

--- teaspils_backend/alerts.py
+++ teaspils_backend/alerts.py
@@ -9,13 +9,13 @@ alerts_plantHistory = [{"text":"La temperatura ha augmentat un 8% respecte la se
                     {"text":"La humitat mínima ha estat de 22.72%","icon":"fa-thermometer-empty"},
                     {"text":"Hi ha 3 pics en la humitat del sòl que coincideixen amb 3 davallades en la humitat ambiental","icon":"fa-arrows-alt-v"}]
 
-advice_plantHistory = [{"text":"La temperatura ha augmentat un 8% respecte la setmana passada; comprova la ubicació de la planta","icon":"fa-thermometer-three-quarters"},
+advice_plantHistory = [{"text":"La temperatura ha augmentat un 8% respecte la setmana passada; comprova la ubicació de la planta, ja que pot canviar la temperatura","icon":"fa-thermometer-three-quarters"},
                     {"text":"L'última observació es va introduïr fa 5 dies; recorda regar prou la planta","icon":"fa-calendar"},
-                    {"text":"La temperatura més alta d'ahir va ser a les 12.30 h; compte amb les hores més caluroses del dia","icon":"fa-clock"},
+                    {"text":"La temperatura més alta d'ahir va ser a les 12.30 h; si la temperatura és massa alta, la planta pot morir","icon":"fa-clock"},
                     {"text":"Hi ha hagut 3 pics en la humitat del sòl durant aquests dies; és quan has regat la planta?","icon":"fa-water"},
                     {"text":"La mesura màxima pel CO2 ha estat 800 ppm; probablement hi hagi hagut una ventil·lació creuada obrint portes i finestres","icon":"fa-exclamation-circle"},
                     {"text":"Hi ha hagut 3 davallades en el CO2; has obert només la finestra?","icon":"fa-arrow-circle-down"},
-                    {"text":"La il·luminació màxima ha estat de 10 lux; assegura’t que la llum solar no sigui massa directa","icon":"fa-sun"},
+                    {"text":"La il·luminació màxima ha estat de 10 lux; assegura’t que la llum solar no sigui massa directa i faci malbé les fulles","icon":"fa-sun"},
                     {"text":"Hi ha 2 davallades que coincideixen al comparar la temperatura i la il·luminació; pot ocorrer a la nit","icon":"fa-arrow-circle-down"},
                     {"text":"La humitat mínima ha estat de 22.72%; assegura’t que la planta estigui en un lloc amb la humitat adequada","icon":"fa-thermometer-empty"},
                     {"text":"Hi ha 3 pics en la humitat del sòl que coincideixen amb 3 davallades en la humitat ambiental; comprova si es deu a que has regat la planta","icon":"fa-arrows-alt-v"}]

04b298f942c9acd741249f14ae79d0cd96d23686
Ariel Ortiz <ariel.ortiz@upf.edu>
2022-08-23 20:12:40 +0200

    Change port

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def main():
         from django.core.management import execute_from_command_line
         # Override default port for `runserver` command
         from django.core.management.commands.runserver import Command as runserver
-        runserver.default_port = "8000"
+        runserver.default_port = "80"
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "

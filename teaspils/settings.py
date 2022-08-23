3df01bed89795480723fe33d9746a4ef25d28677
Ariel Ortiz <ariel.ortiz@upf.edu>
2022-08-23 21:09:48 +0200

    Turning back to mysql

--- teaspils/settings.py
+++ teaspils/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'teaspils.wsgi.application'
 #         }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'teaspils_aws_db',
-#         'USER': 'admin',
-#         'PASSWORD': '--Teaspils22',
-#         'PORT': '3306',
-#         'HOST': 'teaspils-aws-db.c9d6fszohvgi.eu-west-3.rds.amazonaws.com',
-#         }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}   
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'teaspils_aws_db',
+        'USER': 'admin',
+        'PASSWORD': '--Teaspils22',
+        'PORT': '3306',
+        'HOST': 'teaspils-aws-db.c9d6fszohvgi.eu-west-3.rds.amazonaws.com',
+        }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }   
 
 
 # Password validation
